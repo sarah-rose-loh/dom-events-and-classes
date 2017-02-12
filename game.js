c686cb346fa7732b27ca6f0fbdddc793d49d7fd8
Sarah Loh <sarahroseloh@gmail.com>
2017-02-12 21:59:15 +1300

    added make green function

--- game.js
+++ game.js
@@ -13,6 +13,9 @@ function bindEventListeners (dots) {
     // The first one is provided for you
     dots[i].addEventListener('contextmenu', makeGreen)
   }
+  for (var i= 0; i< dots.length; i++) {
+    dots[i].addEventListener('click', makeBlue)
+  }
 }
 
 function makeGreen (evt) {
@@ -22,7 +25,11 @@ function makeGreen (evt) {
 }
 
 // CREATE FUNCTION makeBlue HERE
+function makeBlue (evt){
 
+  evt.target.classList.toggle('blue')
+  updateCounts()
+}
 // CREATE FUNCTION hide HERE
 
 function updateCounts () {
